3b243fecfc5c918dc34b0a42552fae771c239797
Narinderpal Kaleka <narinderpalkaleka@gmail.com>
2022-03-06 13:37:41 -0500

    Header section complete

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-import logo from "./logo.svg";
-import "./App.css";
+import Header from "./components/Header/Header";
 
 function App() {
-  return <div className="App">Initial app</div>;
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
 }
 
 export default App;
