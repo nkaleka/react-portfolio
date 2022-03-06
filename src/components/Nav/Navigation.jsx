a8e4577b73e781784c2765d4a281b30c59c2560e
Narinderpal Kaleka <narinderpalkaleka@gmail.com>
2022-03-06 15:19:44 -0500

    Navigation section complete

---
+++ src/components/Nav/Navigation.jsx
@@ -0,0 +1,21 @@
+import "./Navigation.css";
+const Navigation = () => {
+  return (
+    <ul className="navigation">
+      <li>
+        <a href="#">Home</a>
+      </li>
+      <li>
+        <a href="#">Work</a>
+      </li>
+      <li>
+        <a href="#">Testimonials</a>
+      </li>
+      <li>
+        <a href="#">About</a>
+      </li>
+    </ul>
+  );
+};
+
+export default Navigation;
