342af115e73ca24de37de6ec1056e84719045cdb
Narinderpal Kaleka <narinderpalkaleka@gmail.com>
2022-03-06 15:19:44 -0500

    Navigation section complete

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,11 @@
 import "./Header.css";
-
+import Navigation from "../Nav/Navigation";
 const Header = () => {
-  return <div className="header"></div>;
+  return (
+    <div class="wrapper">
+      <Navigation />
+    </div>
+  );
 };
 
 export default Header;
